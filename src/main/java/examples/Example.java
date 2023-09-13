99edf4588158491760bb455ef97ccb8a0efce059
Tomas Balaz <balaztomas0@gmail.com>
2023-09-13 14:48:03 +0200

    Explanation why java.lang.ClassCastException happens

--- src/main/java/examples/Example.java
+++ src/main/java/examples/Example.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Example {
+
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList();
-        numbers.add(1);
-//        numbers.add("2");
+
+        //Exception in thread "main" java.lang.ClassCastException
+        List numbers = new ArrayList();
+      //  numbers.add(1);
+        numbers.add("2");
+
         for (Object number : numbers) {
             System.out.println((String) number);
         }
