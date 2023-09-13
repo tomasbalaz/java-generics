04ae8d58efe8c8f010223874403a643cfb08e171
Tomas Balaz <balaztomas0@gmail.com>
2023-09-13 14:53:41 +0200

    The second example why java.lang.ClassCastException happens

--- src/main/java/examples/Example2.java
+++ src/main/java/examples/Example2.java
@@ -4,7 +4,8 @@ public class Example2 {
     public static void main(String[] args) {
 //        Comparable number = 10;
 //        number.compareTo("10");
+
         Comparable<Integer> number = 10;
-        System.out.println(number.compareTo(9));
+        System.out.println(number.compareTo(10));
     }
 }
