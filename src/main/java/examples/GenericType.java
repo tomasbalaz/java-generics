2598a45e1e22c7674b2ce9a32da84b2d1b445dfc
Tomas Balaz <balaztomas0@gmail.com>
2023-09-14 13:38:22 +0200

    Explanation of type parameter and type argument

--- src/main/java/examples/GenericType.java
+++ src/main/java/examples/GenericType.java
@@ -8,28 +8,21 @@ import java.util.Map;
 
 public class GenericType {
     public static void main(String[] args) {
-//      List<E>
-        List<String> names =
-                new ArrayList<>();
+
+        // List<E> -> type parameter
+        List<String> names // -> type argument
+                = new ArrayList<String>();
         names.add("1");
-        List<Integer> numbers =
-                new ArrayList<>();
+
+
+        List<Integer> numbers
+                = new ArrayList<>();
         numbers.add(1);
 
-//      Map<K, V>
-        Map<String, String> map =
-                new HashMap<>();
+        Map<String, String> map
+                = new HashMap<>();
 
-        Map<Point, Double> map2 =
-                new HashMap<>();
-
-        /*
-            Naming Convention
-            E – Element (used extensively by the Java Collections Framework, for example ArrayList, Set etc.)
-            K – Key (Used in Map)
-            N – Number
-            T – Type
-            V – Value (Used in Map)
-         */
+        Map<Point, Double> map2
+                = new HashMap<>();
     }
 }
