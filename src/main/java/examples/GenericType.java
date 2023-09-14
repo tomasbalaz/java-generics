7fe3ab173ebbf38f9a9de18a1889213d2b193a24
Tomas Balaz <balaztomas0@gmail.com>
2023-09-14 13:46:59 +0200

    Explanation of Generic Type Naming Convention

--- src/main/java/examples/GenericType.java
+++ src/main/java/examples/GenericType.java
@@ -19,10 +19,22 @@ public class GenericType {
                 = new ArrayList<>();
         numbers.add(1);
 
+        // Map<K,V>
         Map<String, String> map
                 = new HashMap<>();
 
         Map<Point, Double> map2
                 = new HashMap<>();
+
+        /*
+
+        Naming Convention
+        E – Element (used extensively by the Java Collections Framework, for example ArrayList, Set etc.)
+        K – Key (Used in Map)
+        N – Number
+        T – Type
+        V – Value (Used in Map)
+
+         */
     }
 }
