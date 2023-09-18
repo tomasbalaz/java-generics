c3929a172b84ed5be9e7e01395736e59cadbe973
Tomas Balaz <balaztomas0@gmail.com>
2023-09-18 15:21:22 +0200

    Explanation of Bounded Type Parameters in method

--- src/main/java/examples/BoundedTypeParameter.java
+++ src/main/java/examples/BoundedTypeParameter.java
@@ -3,22 +3,14 @@ package examples;
 public class BoundedTypeParameter {
     public static void main(String[] args) {
         Integer[] numbers = {1, 2, 3};
-        System.out.println(
-                countGreaterThan(numbers, 0)
-        );
+        System.out.println(countGreaterThan(numbers, 2));
 
-        Double[] numbers2 = {1.0, 2.0, 3.8};
-        System.out.println(
-                countGreaterThan(numbers2, .9)
-        );
+        Double[] numbers2 = {1.0, 2.0, 3.0};
+        System.out.println(countGreaterThan(numbers2, 0.0));
     }
 
-    interface A {}
-    interface B {}
+    static <T extends Comparable<T>> int countGreaterThan(T[] numbers, T number) {
 
-    static <T extends Comparable<T>> int countGreaterThan(
-            T[] numbers,
-            T number) {
         int count = 0;
         for (T n : numbers) {
             if (n.compareTo(number) > 0) {
