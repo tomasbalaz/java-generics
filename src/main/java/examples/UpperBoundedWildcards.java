773760f5df41171de21e9876562e9f6482b41c4c
Tomas Balaz <balaztomas0@gmail.com>
2023-09-19 15:10:10 +0200

    Explanation of Upper Bounded Wildcards

--- src/main/java/examples/UpperBoundedWildcards.java
+++ src/main/java/examples/UpperBoundedWildcards.java
@@ -7,9 +7,11 @@ public class UpperBoundedWildcards {
     public static void main(String[] args) {
         // ? Wildcards
         // Upper Bounded Wildcards
-        List<Double> list1 = Arrays.asList(1.9, 1.3);
+
+        List<Double> list1 = Arrays.asList(1.0, 2.0);
         List<Integer> list2 = Arrays.asList(1, 2);
         List<Number> list3 = Arrays.asList();
+
         printNumbers(list1);
         printNumbers(list2);
         printNumbers(list3);
@@ -17,8 +19,8 @@ public class UpperBoundedWildcards {
 
     static void printNumbers(List<? extends Number> list) {
         list.forEach(e -> {
-            System.out.println(e.getClass().getName());
             System.out.println(e);
+            System.out.println(e.getClass().getName());
         });
     }
 }
